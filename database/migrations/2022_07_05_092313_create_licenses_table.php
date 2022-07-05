a32f4d7217b3b48c06c9d7e936c1f0f3a7c3075e
Dc dev <dcmanagertech1121@gmail.com>
2022-07-05 05:24:11 -0400

    default login & register completed

--- database/migrations/2022_07_05_092313_create_licenses_table.php
+++ database/migrations/2022_07_05_092313_create_licenses_table.php
@@ -13,17 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('licenses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('user_role')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
-
     }
 
     /**
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('licenses');
     }
 };
